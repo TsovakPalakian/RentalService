fd9dce9c588b54c6da906f611772340ca12a0d15
TsovakPalakian <tsovakpalakian@gmail.com>
2017-04-01 11:00:53 +0300

    Rental service

--- by/htp/rentalservice/entity/OptionalAccessories.java
+++ by/htp/rentalservice/entity/OptionalAccessories.java
@@ -56,4 +56,53 @@ public class OptionalAccessories extends Equipment {
 	public void setSex(boolean sex) {
 		this.sex = sex;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + (sex ? 1231 : 1237);
+		result = prime * result + sizeEquip;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OptionalAccessories other = (OptionalAccessories) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		if (sex != other.sex)
+			return false;
+		if (sizeEquip != other.sizeEquip)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
 }
--- by/htp/rentalservice/logic/HashMap.java
+++ by/htp/rentalservice/logic/HashMap.java
@@ -1,5 +1,33 @@
 package by.htp.rentalservice.logic;
 
-public interface HashMap {
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Set;
 
+public class HashMap extends AbstractMap<K,V> implements Map<K,V> {
+
+	@Override
+	public V get(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<K, V>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
